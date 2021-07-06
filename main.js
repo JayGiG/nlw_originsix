9a90617d5c8e04f7aa4eacd57d0d9ad7c3f8443d
Janiele Nogueira <njaniele@yahoo.com>
2021-07-06 18:59:34 -0300

    evento de click para esconder menu

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+// Abre e fecha o menu ao clicar no icone 
+
 const nav = document.querySelector('#header nav');
 const toggle = document.querySelectorAll('nav .toggle');
 
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show');
+  })
+}
+
+
+// Ao clicar em um item do menu, esconde o menu
+const links = document.querySelectorAll('nav ul li a');
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show');
   })
 }
